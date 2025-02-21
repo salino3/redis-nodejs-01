8e8974318c5a895ae67459755b5fcee8a93fd9a2
Salino3 <flaviobcn012@gmail.com>
2025-02-21 17:12:47 +0100

    running server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import axios from "axios";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.get("/characters", async (req, res) => {
   const { data } = await axios.get("https://rickandmortyapi.com/api/character");
   return res?.json(data);
 });
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
+app.get("/", async (req, res) => {
+  res.send("Welcome!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
 });
