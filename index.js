7243cf104cf6856bda89c84219635b84b5eb8b4b
Salino3 <flaviobcn012@gmail.com>
2025-02-21 17:19:08 +0100

    commit

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from "express";
 import axios from "axios";
+// library check time endpoint call
+import responseTime from "response-time";
 import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
+
+app.use(responseTime());
 
 app.get("/characters", async (req, res) => {
   const { data } = await axios.get("https://rickandmortyapi.com/api/character");
   return res?.json(data);
 });
 
-app.get("/", async (req, res) => {
+app.get("/ping", async (req, res) => {
   res.send("Welcome!");
 });
 
